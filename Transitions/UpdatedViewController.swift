9a7c40bde4b3a5a3c2e4eb001faa5cb5c5cf6773
Tbone288 <todo3858@740s-pl-1176.local>
2016-10-03 12:33:53 -0600

    Linked segue from Storyboard to ViewController code.

---
+++ Transitions/UpdatedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UpdatedViewController.swift
+//  Transitions
+//
+//  Created by Odom, Tyler on 10/3/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+
+class UpdatedViewController : UIViewController
+{
+    
+}
